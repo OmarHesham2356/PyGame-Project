df826e7050a5f4f4f2f23d3535765f18d20ca8ca
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2023-12-10 10:09:20 +0200

    Update command.py

--- command.py
+++ command.py
@@ -2,10 +2,13 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot("NUians")
+
 traninig = [
     "Hello",
     "Hi There Nuian",
     "How are you doing",
     "I am doing well",
-    
+    "That is good to hear"
+
 ]
+
