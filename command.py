527ee4c49e257f243c706f5864a43b3683f9e464
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2023-12-10 10:04:27 +0200

    i added some training conditions

--- command.py
+++ command.py
@@ -1,2 +1,11 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
+chatbot = ChatBot("NUians")
+traninig = [
+    "Hello",
+    "Hi There Nuian",
+    "How are you doing",
+    "I am doing well",
+    
+]
