29703b7a3a512b8f958c0e9ed19c83524bf07224
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2023-12-12 22:01:21 +0200

    Delete command.py

--- command.py
+++
@@ -1,8 +0,0 @@
-import spacy
-from spacy.matcher import Matcher
-
-# Load the language model
-nlp = spacy.load('en_core_web_sm')
-
-# Initialize the matcher with the shared vocab
-matcher = Matcher(nlp.vocab)
