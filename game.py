3fc6a30630e8d54f2366629c08b3c0c581d8d176
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2023-12-13 14:22:48 +0200

    i added few codes

--- game.py
+++ game.py
@@ -8,4 +8,17 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("SPACE DODGE")
  
 
- 
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
