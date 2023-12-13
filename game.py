4aa0c07dcbe44e728ec14428c1ce994286136dda
ZiaD <143344858+ZiaDz999@users.noreply.github.com>
2023-12-13 19:22:11 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -6,19 +6,18 @@ WIDTH ,HEIGHT=1000,800
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("SPACE DODGE")
- 
+
+BG= pygame.image.load("bg.jpg")
+
+def draw():
+    WIN.blit(BG,(0,0))
+    pygame.display.update()
 
 def main():
     run = True
 
     while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get ():
+            if event.type==pygame.QUIT:
                 run = False
-                break
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+                break 
